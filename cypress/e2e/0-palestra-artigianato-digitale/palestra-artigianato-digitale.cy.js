03ae35a03da745c08bf18f26580ae72c2074c497
Marco Matarazzi <marco.matarazzi86@gmail.com>
2022-07-01 17:00:56 +0200

    sample test BE staging

--- cypress/e2e/0-palestra-artigianato-digitale/palestra-artigianato-digitale.cy.js
+++ cypress/e2e/0-palestra-artigianato-digitale/palestra-artigianato-digitale.cy.js
@@ -2,15 +2,39 @@ context('Palestra artigianato digitale', () => {
 
   it('Primo test', () => {
 
-    cy.origin('https://slope.it', () => {
+    cy.origin('https://booking.staging.slope.it/24ee90d9-2432-4d0d-aeec-8ce845fa18da', () => {
 
       cy.visit('/');
 
-      cy.get('main')
-        .should('contain', 'Slope')
-        .should('contain', 'Software gestionale hotel');
+      cy.get('.banner-box')
+        .should('contain', 'Welcome to Relais Villasole');
 
-      cy.get('header a.raven-button').last().click(); // Click on "Richiedi demo"
+      cy.wait(1000);
+
+      // accetto i cookie
+      cy.get('#accept-explicit-cookie-policy').click();
+
+      cy.wait(1000);
+
+      // apro il modale cookie policy
+      cy.get('a[data-modal-content="cookie-policies-modal"]').first().click();
+
+      cy.wait(1000);
+
+      // chiudo il modale
+      cy.get('.slope-modal-close').click();
+
+      cy.wait(1000);
+
+      cy.get('a[data-lodging-id="bdc93283-2cc0-4b70-a590-3d941d7b7bda"]').click();
+
+      cy.wait(1000);
+
+      cy.get('a[data-rate-or-promotion-id="815dbc7d-9927-43cb-a82d-372ace50e16b"]').click();
+
+
+
+      //cy.get('header a.raven-button').last().click(); // Click on "Richiedi demo"
     })
   });
 })
